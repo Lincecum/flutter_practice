bb1433c4b480d23c550aa3d0413b85bd8613d134
wrjang <wrjang96@gmail.com>
2023-11-07 23:32:51 +0900

    23/11/07 - [플러터] 인스타그램 클론 코딩 #2 - BottomNavigation + History 23:20

--- flutter_clone_instagram/lib/src/app.dart
+++ flutter_clone_instagram/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/image_data.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -6,7 +7,54 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Scaffold(appBar: AppBar(), body: Container()),
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: 0,
+            onTap: (value){
+
+            },
+            items: [
+              BottomNavigationBarItem(
+                // icon: Image.asset('assets/images/add_freind_icon.jpg'),
+                icon: ImageData(IconsPath.homeOff),
+                activeIcon: ImageData(IconsPath.homeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                // icon: Image.asset('assets/images/add_freind_icon.jpg'),
+                icon: ImageData(IconsPath.searchOff),
+                activeIcon: ImageData(IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                // icon: Image.asset('assets/images/add_freind_icon.jpg'),
+                icon: ImageData(IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                // icon: Image.asset('assets/images/add_freind_icon.jpg'),
+                icon: ImageData(IconsPath.activeOff),
+                activeIcon: ImageData(IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                // icon: Image.asset('assets/images/add_freind_icon.jpg'),
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.grey),
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
+        ),
         onWillPop: () async {
           return false;
         });
